b9878ef2a564563dc23d01075a57b4b6a8a6523c
FroZZe <darkfrozen659@gmail.com>
2022-08-08 17:57:20 +0300

    real device script

--- authrealdevice.py
+++ authrealdevice.py
@@ -10,31 +10,45 @@ from selenium.webdriver.support import expected_conditions as EC
 
 class Authorization:
     def __init__(self):
+        self.element = None
         desired_capabilities = {
             "platformName": "Android",
-            "platformVersion": "9",
-            "deviceName": "Android Emulator",
+            "platformVersion": "7",
+            "uid": "2a1958850904",
+            "deviceName": "Xiaomi Redmi Note 4",
             "app": "/home/pavel/Документы/appiutest/st3.apk",
             "noReset": True
         }
         self.driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_capabilities=desired_capabilities)
 
     def log(self):
-        element = WebDriverWait(self.driver, 20).until(
+        lst = []
+        self.element = WebDriverWait(self.driver, 20).until(
             EC.presence_of_element_located((By.ID, "ru.tokyocity.tokyocity.stage3:id/more"))
         )
-        element.click()
+        self.element.click()
+        
         self.driver.implicitly_wait(10)
         self.driver.find_element(By.ID, "ru.tokyocity.tokyocity.stage3:id/loginButton").click()
         self.driver.find_element(By.ID, "ru.tokyocity.tokyocity.stage3:id/phoneEditText").send_keys("1234567894")
         self.driver.find_element(By.ID, "ru.tokyocity.tokyocity.stage3:id/getCodeButton").click()
         self.driver.find_element(By.ID, "ru.tokyocity.tokyocity.stage3:id/editText").send_keys("1234")
+        self.driver.implicitly_wait(15)
+        self.driver.swipe(470, 1400, 470, 950, 330)
+        # lst = self.driver.find_element(By.ID, 'com.abc.android.abc:id/common_tab_list')
+        # action = TouchAction(self.driver)
+        # for i in range(0, 10):
+        #     action.press(x=750, y=1750).move_to(x=0, y=-75).perform()
         self.driver.find_element(By.ID, "ru.tokyocity.tokyocity.stage3:id/logoutButton")
-        # self.scroll_page()
 
-    # def scroll_page(self):
-    #     self.driver.swipe(150, 400, 150, 200, 1000)
+    def get_myWindow_size(self):
+
+        x = self.driver.get_window_size()['width']  # Get the length of the x axis
+        y = self.driver.get_window_size()['height']  # Get the length of the y axis
+
+        return x, y
 
 
 login = Authorization()
+print(login.get_myWindow_size())
 login.log()
