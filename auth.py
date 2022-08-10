82cdc0e62a727c17197c2a3dce84179c2b6c9c3a
FroZZe <darkfrozen659@gmail.com>
2022-08-10 15:56:06 +0300

    Refactoring

--- auth.py
+++ auth.py
@@ -2,21 +2,25 @@ from appium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import pytest
 
 
-class Authorization:
-    def __init__(self):
-        self.element = None
-        desired_capabilities = {
-            "platformName": "Android",
-            "platformVersion": "11",
-            "deviceName": "Android Emulator",
-            "app": "/home/pavel/Документы/appiutest/st3.apk",
-            "noReset": True
-        }
-        self.driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_capabilities=desired_capabilities)
+def driver(self):
+    self.element = None
+    desired_capabilities = {
+        "platformName": "Android",
+        "platformVersion": "11",
+        "deviceName": "Android Emulator",
+        "app": "/home/pavel/Documents/appiutest/st1.apk",
+        "noReset": True
+    }
+    self.driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_capabilities=desired_capabilities)
 
-    def log(self):
+
+class TestAuthorization:
+    @pytest.mark.xfail
+    def test_log_in(self):
+        print('Login test started')
         # geo = WebDriverWait(self.driver, 20).until(
         #     EC.presence_of_element_located((By.ID, "com.android.packageinstaller:id/permission_allow_button"))
         # )
@@ -28,24 +32,31 @@ class Authorization:
         # self.driver.implicitly_wait(10)
         # self.driver.find_element(By.XPATH, "//UIAStaticText[contains(@text ='Санкт-Петербург')]")
         self.element = WebDriverWait(self.driver, 20).until(
-            EC.presence_of_element_located((By.ID, "ru.tokyocity.tokyocity.stage3:id/more"))
+            EC.presence_of_element_located((By.ID, "ru.tokyocity.tokyocity.stage1:id/more"))
         )
         self.element.click()
         self.driver.implicitly_wait(10)
-        self.driver.find_element(By.ID, "ru.tokyocity.tokyocity.stage3:id/loginButton").click()
-        self.driver.find_element(By.ID, "ru.tokyocity.tokyocity.stage3:id/phoneEditText").send_keys("1234567894")
-        self.driver.find_element(By.ID, "ru.tokyocity.tokyocity.stage3:id/getCodeButton").click()
-        self.driver.find_element(By.ID, "ru.tokyocity.tokyocity.stage3:id/editText").send_keys("1234")
+        self.driver.find_element(By.ID, "ru.tokyocity.tokyocity.stage1:id/loginButton").click()
+        self.driver.find_element(By.ID, "ru.tokyocity.tokyocity.stage1:id/phoneEditText").send_keys("1234567894")
+        self.driver.find_element(By.ID, "ru.tokyocity.tokyocity.stage1:id/getCodeButton").click()
+        self.driver.find_element(By.ID, "ru.tokyocity.tokyocity.stage1:id/editText").send_keys("1234")
+        assert True, "Login test failed"
+        print('Login test finished')
 
-        self.driver.implicitly_wait(15)
-        element_to_tap = self.driver.find_element(By.ID, "ru.tokyocity.tokyocity.stage3:id/ordersButton")
-        element_to_drag_to = self.driver.find_element(By.ID, "ru.tokyocity.tokyocity.stage3:id/profileImageView")
+    @pytest.mark.xfail
+    def test_log_out(self):
+        print("Logout test stated")
+        element_to_tap = self.driver.find_element(By.ID, "ru.tokyocity.tokyocity.stage1:id/ordersButton")
+        element_to_drag_to = self.driver.find_element(By.ID, "ru.tokyocity.tokyocity.stage1:id/profileImageView")
         self.driver.scroll(element_to_tap, element_to_drag_to)
         self.driver.implicitly_wait(10)
-        self.driver.find_element(By.ID, "ru.tokyocity.tokyocity.stage3:id/logoutButton").click()
+        self.driver.find_element(By.ID, "ru.tokyocity.tokyocity.stage1:id/logoutButton").click()
         # self.driver.find_element(By.ID, "android:id/button2").click()  #Отмена
-        self.driver.find_element(By.ID, "android:id/button1").click()  #OK
+        self.driver.find_element(By.ID, "android:id/button1").click()  # OK
+        assert True, "Logout test failed"
+        print("Logout tested finished")
 
 
-login = Authorization()
-login.log()
+login = TestAuthorization()
+login.test_log_in()
+login.test_log_out()
